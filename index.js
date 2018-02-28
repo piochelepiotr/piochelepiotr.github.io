c3063ce706babea164d3dc4fe5f8b39ca8616199
Piotr Wolski <piotr.wolski@telecom-paristech.fr>
2018-02-28 11:45:08 +0000

    Added node js server to project.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express()
+var fs = require("fs");
+
+app.get('/', function (req, res) {
+    fs.readFile("index.html", function(err, data) {
+        res.write(data);
+        res.end();
+    });
+})
+
+app.get('*', function (req, res) {
+  res.send('Invalid URL')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
