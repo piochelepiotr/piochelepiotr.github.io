d838b259935a90b3bad073d0116147813b9b1fe3
Piotr Wolski <piotrwolski@Piotrs-MacBook-Pro.local>
2018-03-02 12:33:10 +0000

    Added continuous deployment.

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get('*', function (req, res) {
 })
 
 app.listen(8081, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8081!')
 })
